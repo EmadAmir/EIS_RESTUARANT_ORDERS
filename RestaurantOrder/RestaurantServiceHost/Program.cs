3b3d6531213a26d4e31c9a589ab4869d3ebba12a
Emad Amir <24242.imad@gmail.com>
2019-04-07 01:55:42 +0100

    created the WCF service but getting this error System.ServiceModel.AddressAccessDeniedException: 'HTTP could not register URL http://+:50045/. Your process does not have access rights to this namespace (see http://go.microsoft.com/fwlink/?LinkId=70353 for details).'

---
+++ RestaurantOrder/RestaurantServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace RestaurantServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(RestaurantService.RestaurantService)))
+            {
+                host.Open();
+                Console.WriteLine("Host has started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
