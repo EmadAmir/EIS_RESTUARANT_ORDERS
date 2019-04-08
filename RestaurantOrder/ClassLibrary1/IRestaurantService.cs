56fc72f10144a3261767da090288d5c50247c862
Emad Amir <24242.imad@gmail.com>
2019-04-08 21:45:49 +0100

    created database using sql server

--- RestaurantOrder/ClassLibrary1/IRestaurantService.cs
+++ RestaurantOrder/ClassLibrary1/IRestaurantService.cs
@@ -8,8 +8,8 @@ using System.Text;
 namespace RestaurantService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestaurantServiceWCF" in both code and config file together.
-    [ServiceContract]
-    public interface IRestaurantService
+    [ServiceContract(Name = "ABCD")]
+    public interface IRestaurantServices
     {
         [OperationContract]
         string GetMessage(string name);
--- RestaurantOrder/ClassLibrary1/RestaurantService.cs
+++ RestaurantOrder/ClassLibrary1/RestaurantService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace RestaurantService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestaurantServiceWCF" in both code and config file together.
-    public class RestaurantService : IRestaurantService
+    public class RestaurantService : IRestaurantServices
     {
         //public void DoWork()
         //{
